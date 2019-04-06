704f4861ced0f6d57a6a5b93638a18e994a82c59
morswin22 <xmorswinx@gmail.com>
2019-04-06 14:02:56 +0200

    Enchanced bubble sort speed slider

--- bubblesort/sketch.js
+++ bubblesort/sketch.js
@@ -3,6 +3,7 @@ let j = 0;
 
 let wV = 10;
 let speedV = 60;
+let iters = 1;
 let showSelectedV = false;
 
 let values;
@@ -21,10 +22,15 @@ function setup() {
     init();
   });
 
-  speed = createSlider(1, 60, speedV, 1);
+  speed = createSlider(1, 120, speedV, 1);
   speed.changed(()=>{
     speedV = speed.value();
     frameRate(speedV);
+    if (speedV > 60) {
+      iters = map(speedV, 60, 120, 1, 20);
+    } else {
+      iters = 1;
+    }
   });
 
   showSelected = createCheckbox("Show selected?", showSelectedV);
@@ -53,20 +59,22 @@ function init() {
 function draw() {
   background(51);
 
-  let a = values[j];
-  let b = values[j+1];
-  if (a > b) {
-    swap(values, j, j+1);
-  }
-
-  if (i < values.length) {
-    j++;
-    if (j>=values.length-i-1) {
-      j = 0;
-      i++;
+  for (let iter = 0; iter < iters; iter++) {
+    let a = values[j];
+    let b = values[j+1];
+    if (a > b) {
+      swap(values, j, j+1);
     }
-  } else {
+
+    if (i < values.length) {
+      j++;
+      if (j>=values.length-i-1) {
+        j = 0;
+        i++;
+      }
+    } else {
     noLoop();
+    }
   }
 
   for (let i = 0; i < values.length; i++) {
